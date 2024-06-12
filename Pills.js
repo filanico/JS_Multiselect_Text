2c80b6aa4eaee2b6c3acda02ebd67aa8e1a959d8
Nicola Antonio Filannino <z.nicola.filannino@guest.rcs.it>
2024-06-12 10:17:45 +0200

    On Pill_onBeforeAppendSelectedItem trigger, we pass listItem

--- Pills.js
+++ Pills.js
@@ -53,7 +53,7 @@ function Pills(cssSelector, JSconfig={}) {
     pill.addEventListener('click',function(){
       this.trigger('Pill_onSelectedItemClick',{listItem:node, pill,root})
     }.bind(this))
-    this.trigger('Pill_onBeforeAppendSelectedItem',{entry,node,root});
+    this.trigger('Pill_onBeforeAppendSelectedItem',{listItem:node, pill,root});
     node.parentNode.parentNode.append(pill);
     this.trigger('Pill_onListItemClick',{pill,root})
   }.bind(this);
